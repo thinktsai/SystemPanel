6279d106bccfdbba01075677d0d36d274be0d3cc
thinktsai <steve@synso.com>
2017-08-22 11:32:41 +0800

    add virtual keyboard

--- app/src/main/java/com/ctos/systempanel/SignupActivity.java
+++ app/src/main/java/com/ctos/systempanel/SignupActivity.java
@@ -1,16 +1,22 @@
 package com.ctos.systempanel;
 
+import android.app.Activity;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.inputmethodservice.Keyboard;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import android.inputmethodservice.Keyboard;
+import android.inputmethodservice.KeyboardView;
 
 
 public class SignupActivity extends AppCompatActivity {
@@ -21,11 +27,19 @@ public class SignupActivity extends AppCompatActivity {
     EditText reEnterPasswordText;
     FloatingActionButton changeButton;
     FloatingActionButton cancelButton;
+    private Keyboard mKeyboard;
+    private CustomKeyboardView mKeyboardView;
+    private Activity main;
+    private final int MIN_LENGTH_PASSWD = 8;
+    private final int MAX_LENGTH_PASSWD = 12;
+
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        main = this;
 
         oldPasswordText = (EditText) findViewById(R.id.input_oldPassword);
         passwordText = (EditText) findViewById(R.id.input_password);
@@ -33,6 +47,35 @@ public class SignupActivity extends AppCompatActivity {
 
         changeButton = (FloatingActionButton) findViewById(R.id.button_change);
         cancelButton = (FloatingActionButton) findViewById(R.id.button_cancel);
+
+
+        View.OnTouchListener touchListener = new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View v, MotionEvent event) {
+                v.onTouchEvent(event);
+                mKeyboardView.showCustomKeyboard();
+                return true;
+            }
+        };
+
+        View.OnFocusChangeListener listener = new View.OnFocusChangeListener() {
+            public void onFocusChange(View v, boolean hasFocus) {
+                if (mKeyboardView.getVisibility() == View.GONE)
+                    mKeyboardView.showCustomKeyboard();
+            }
+        };
+
+        mKeyboard = new Keyboard(this, R.xml.keyboard);
+        mKeyboardView = (CustomKeyboardView) findViewById(R.id.keyboard_view);
+        mKeyboardView.setKeyboard(mKeyboard);
+        mKeyboardView.setOnKeyboardActionListener(new BasicOnKeyboardActionListener(this));
+        mKeyboardView.randomizeKeys();
+        oldPasswordText.setOnFocusChangeListener(listener);
+        passwordText.setOnFocusChangeListener(listener);
+        reEnterPasswordText.setOnFocusChangeListener(listener);
+        oldPasswordText.setOnTouchListener(touchListener);
+        passwordText.setOnTouchListener(touchListener);
+        reEnterPasswordText.setOnTouchListener(touchListener);
 
 
         changeButton.setOnClickListener(new View.OnClickListener() {
@@ -52,6 +95,12 @@ public class SignupActivity extends AppCompatActivity {
 //                overridePendingTransition(R.anim.push_left_in, R.anim.push_left_out);
             }
         });
+    }
+
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if (keyCode == KeyEvent.KEYCODE_TAB)
+            mKeyboardView.hideCustomKeyboard();
+        return super.onKeyDown(keyCode, event);
     }
 
     public void signup() {
@@ -86,7 +135,7 @@ public class SignupActivity extends AppCompatActivity {
                         // onSignupFailed();
                         progressDialog.dismiss();
                     }
-                }, 3000);
+                }, 1000);
     }
 
 
@@ -109,7 +158,7 @@ public class SignupActivity extends AppCompatActivity {
         String password = passwordText.getText().toString();
         String reEnterPassword = reEnterPasswordText.getText().toString();
 
-        if (oldPassword.isEmpty() || password.length() < 4 || password.length() > 10) {
+        if (oldPassword.isEmpty() || password.length() < MIN_LENGTH_PASSWD || password.length() > MAX_LENGTH_PASSWD) {
             oldPasswordText.setError("between 4 and 10 alphanumeric characters");
             valid = false;
         } else if (!MainActivity.ctosInfo.getPasswordHash(oldPassword).equals(MainActivity.ctosInfo.getPasswordHash())) {
@@ -120,14 +169,15 @@ public class SignupActivity extends AppCompatActivity {
         }
 
 
-        if (password.isEmpty() || password.length() < 4 || password.length() > 10) {
+        if (password.isEmpty() || password.length() < MIN_LENGTH_PASSWD || password.length() > MAX_LENGTH_PASSWD) {
             passwordText.setError("between 4 and 10 alphanumeric characters");
             valid = false;
         } else {
             passwordText.setError(null);
         }
 
-        if (reEnterPassword.isEmpty() || reEnterPassword.length() < 4 || reEnterPassword.length() > 10 || !(reEnterPassword.equals(password))) {
+        if (reEnterPassword.isEmpty() || reEnterPassword.length() < MIN_LENGTH_PASSWD
+                || reEnterPassword.length() > MAX_LENGTH_PASSWD || !(reEnterPassword.equals(password))) {
             reEnterPasswordText.setError("Password Do not match");
             valid = false;
         } else {
